60713c4f0de5e8a3f029fe2f9401bf9cab7fc997
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2019-09-25 13:07:05 +0500

    Update models.py
    Добавил вариант со списком.

--- app/models.py
+++ app/models.py
@@ -91,13 +91,14 @@ class Post(db.Model):
     board_name = db.Column(db.String(10), db.ForeignKey('board.ref'))
     answers = db.Column(db.String(100))
     # или просто сериализируем
-#     _ratings = Column(db.String, default='0.0')
-#     @property
-#     def ratings(self):
-#         return [float(x) for x in self._ratings.split(';')]
-#     @ratings.setter
-#     def ratings(self, value):
-#         self._ratings += ';%s' % value
+    #_ratings = Column(db.String, default='0.0')
+    @property
+    def _answers(self):
+        return json.loads(self.answers)
+    @ratings.setter
+    def _answers(self, value):
+        self.answers = json.dumps(value)
+    
     reply = db.relationship(
         'Post', secondary=answers,
         primaryjoin=(answers.c.answ_from_id == id),
