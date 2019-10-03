79f0eaacbf781c003f663f68e724d62361556183
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2019-10-03 15:36:10 +0500

    Добавил авторизацию админов.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 # from hashlib import md5
-from app import db  # , login
+from app import db, login
 import json
-# from flask_login import UserMixin
-# from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # followers = db.Table(
@@ -13,14 +13,15 @@ import json
 # )
 
 
-# class User(db.Model):  # UserMixin,
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, unique=True)
-#     email = db.Column(db.String(120), index=True, unique=True)
-#     password_hash = db.Column(db.String(128))
-#     posts = db.relationship('Post', backref='author', lazy='dynamic')
-#     about_me = db.Column(db.String(140))
-#     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+class User(UserMixin, db.Model):  #
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    # email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    access_level = db.Column(db.Integer)
+    # posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # about_me = db.Column(db.String(140))
+    # last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     # followed = db.relationship(
     #     'User', secondary=followers,
@@ -28,14 +29,14 @@ import json
     #     secondaryjoin=(followers.c.followed_id == id),
     #     backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
-    # def set_password(self, password):
-    #     self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    # def check_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # def avatar(self, size):
     #     digest = md5(self.email.lower().encode('utf-8')).hexdigest()
@@ -62,9 +63,9 @@ import json
     #     return followed.union(own).order_by(Post.timestamp.desc())
 
 
-# @login.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 # class Post(db.Model):
