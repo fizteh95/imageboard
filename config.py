eeb1557291dfc0893a3758bb258ff98d56006f60
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2019-10-14 12:15:27 +0500

    Готовим конфиг к хероку.

--- config.py
+++ config.py
@@ -8,11 +8,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # ADMINS = ['your-email@example.com']
-    POSTS_PER_PAGE = 25
+    # POSTS_PER_PAGE = 25
     PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=365)
