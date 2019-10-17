eb8f0721311adc15cd4936b082bbb99d92e5e589
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2019-10-17 13:42:02 +0500

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,6 @@ from flask_login import login_user, logout_user, current_user, login_required
 # from werkzeug.urls import url_parse
 # from app import app, db
 # from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
-# from app.models import User, Post
 
 
 
