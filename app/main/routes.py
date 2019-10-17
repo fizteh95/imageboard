97d8b09ebe7ff2f168dde85214c2744753df9b46
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2019-10-17 09:24:05 +0500

    Быстрофикс косяков.

--- app/main/routes.py
+++ app/main/routes.py
@@ -75,7 +75,7 @@ def admin():
         db.session.add(u)
         db.session.commit()
         
-    boards = Board
+    boards = Board.query.all()
     if len(boards) == 0:
         b = Board(ref='b', description='Random talks')
         db.session.add(b)
@@ -91,7 +91,7 @@ def admin():
  
  
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -101,7 +101,7 @@ def admin():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
